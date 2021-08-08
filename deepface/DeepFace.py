0901b52d1769bad867370b27a07d367f08425c97
IftikharulSami <49338960+IftikharulSami@users.noreply.github.com>
2021-08-08 14:36:58 +0500

    Update DeepFace.py

--- deepface/DeepFace.py
+++ deepface/DeepFace.py
@@ -12,7 +12,7 @@ import pandas as pd
 from tqdm import tqdm
 import pickle
 
-from deepface.basemodels import VGGFace, OpenFace, Facenet, Facenet512, FbDeepFace, DeepID, DlibWrapper, ArcFace, Boosting
+from deepface.basemodels import VGGFace, OpenFace, Facenet, Facenet512, FbDeepFace, DeepID, DlibWrapper, ArcFace# Boosting
 from deepface.extendedmodels import Age, Gender, Race, Emotion
 from deepface.commons import functions, realtime, distance as dst
 
