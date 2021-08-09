f3bd53848ab27ef3c9fd528e62a85d08a3c0192f
IftikharulSami <49338960+IftikharulSami@users.noreply.github.com>
2021-08-10 00:31:54 +0500

    Update Facenet.py

--- deepface/basemodels/Facenet.py
+++ deepface/basemodels/Facenet.py
@@ -555,17 +555,18 @@ def loadModel(url = 'https://github.com/serengil/deepface_models/releases/downlo
 
 	#-----------------------------------
 
-	home = str(Path.home())
+# 	home = str(Path.home())
 
-	if os.path.isfile(home+'/.deepface/weights/facenet_weights.h5') != True:
-		print("facenet_weights.h5 will be downloaded...")
+# 	if os.path.isfile(home+'/.deepface/weights/facenet_weights.h5') != True:
+# 		print("facenet_weights.h5 will be downloaded...")
 
-		output = home+'/.deepface/weights/facenet_weights.h5'
-		gdown.download(url, output, quiet=False)
+# 		output = home+'/.deepface/weights/facenet_weights.h5'
+# 		gdown.download(url, output, quiet=False)
 
 	#-----------------------------------
 
-	model.load_weights(home+'/.deepface/weights/facenet_weights.h5')
+	model.load_weights('model_weights/facenet_weights.h5')
+# 	model.load_weights(home+'/.deepface/weights/facenet_weights.h5')
 
 	#-----------------------------------
 
