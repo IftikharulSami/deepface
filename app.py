2d5f0c97f05ddcdadaae6a1278995773f69d8864
IftikharulSami <49338960+IftikharulSami@users.noreply.github.com>
2021-08-12 14:39:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
 from flask import Flask, render_template, Response, request, url_for
 import os
+import  numpy as np
+# from services import FR_Services
+import timeit
 import tensorflow as tf
 from tensorflow.python.keras.backend import set_session
-from services import FR_Services
+from deepface.basemodels import Facenet512
 from deepface import DeepFace
-import timeit
-from deepface.basemodels import OpenFace
 import faiss
-import numpy as np
+
 
 graph = tf.get_default_graph()
-
-app = Flask(__name__)
-sess = tf.Session()
-set_session(sess)
-my_model = OpenFace.loadModel()
-ser = FR_Services()
-
 train_emb = np.load('weights/train_emb.npy')
 (dim_0, dim_1) = np.shape(train_emb)
 train_names = np.load('weights/train_names.npy')
 index =  faiss.IndexFlatL2(dim_1)
-# def load_model():
-#     global model
-#     model = Facenet512.loadModel('model_weights/facenet512_weights.h5')
+index.add(train_emb)
+print(index.ntotal)
+
+app = Flask(__name__)
+# ser = FR_Services()
+
+
+sess = tf.Session()
+set_session(sess)
+my_model = Facenet512.loadModel()
+
+
 def face_recognize(test_image):
     global graph
     global sess
     print('Model Loaded.............')
+    path = f'upload_Images/{test_image}'
     t_start = timeit.default_timer()
     with graph.as_default():
         set_session(sess)
-        tst_emb = DeepFace.represent(f'upload_Images/{test_image}', model=my_model ,detector_backend='mtcnn')
+        tst_emb = DeepFace.represent(path, model=my_model ,detector_backend='mtcnn')
     t_end = timeit.default_timer()
     print(f'Model Loading Time= {t_end-t_start}')
     print('Embedding Generated.............')
@@ -39,10 +43,10 @@ def face_recognize(test_image):
     print('DType Converted.............')
     tst_emb = np.reshape(tst_emb, (1, dim_1))
     print('Array Reshaped.............')
-    # os.remove(f'upload_Images/{test_image}')
-    print('Image File Deleted.............')
+    os.remove(path)
+    if not path:
+        print('Image File Deleted.............')
     # print(self.train_emb.dtype)
-    index.add(train_emb)
     D, I = index.search(tst_emb, 3)
     print(I)
     idx = I[0][0]
@@ -50,11 +54,10 @@ def face_recognize(test_image):
     label = train_names[idx]
     return label, distance
 
+
 # app.config['Upload_Images'] = Upload
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    global graph
-    global sess
     if request.method == 'POST':
         if request.files:
             if (request.files['unknown_image'] and not request.files['new_image']):
@@ -69,13 +72,9 @@ def home():
                 new_image = request.files['new_image']
                 value = request.form['new_label']
                 value = value.title()
-#               reply = ser.retrain(new_image.filename, value)
-                return render_template('index.html', reply=reply)
+                # reply = ser.retrain(new_image.filename, value)
+                return render_template('index.html')# , reply=reply
     return render_template('index.html')
-
-
-
-
 
 
 if __name__ == '__main__':
