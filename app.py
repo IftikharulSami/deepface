20fb000dca81671f026b1c33bb9658924d66e9ad
IftikharulSami <49338960+IftikharulSami@users.noreply.github.com>
2021-08-12 14:12:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def home():
     global sess
     if request.method == 'POST':
         if request.files:
-            if (request.files['unknown_image'] and not request.files['new_image']):
-                unknown_image = request.files['unknown_image']
-                print(unknown_image.filename)
-                unknown_image.save(os.path.join('upload_Images', unknown_image.filename))
                 t_start = timeit.default_timer()
                 with graph.as_default():
-                    set_session(sess)
-                    tst_emb = DeepFace.represent(f'upload_Images/{test_image}', model=my_model ,detector_backend='mtcnn')
+                     if (request.files['unknown_image'] and not request.files['new_image']):
+                        unknown_image = request.files['unknown_image']
+                        print(unknown_image.filename)
+                        unknown_image.save(os.path.join('upload_Images', unknown_image.filename))
+                        set_session(sess)
+                        tst_emb = DeepFace.represent(f'upload_Images/{test_image}', model=my_model ,detector_backend='mtcnn')
                 t_end = timeit.default_timer()
                 print(f'Model Loading Time= {t_end-t_start}')
                 return render_template('index.html', label=label, dist=dist)
