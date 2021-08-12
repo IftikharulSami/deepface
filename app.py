c9314dc58dd9171dd1a8ac4a14b8b8fdb1c07386
IftikharulSami <49338960+IftikharulSami@users.noreply.github.com>
2021-08-12 13:18:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, Response, request, url_for
 import os
+import tensorflow as tf
 from services import FR_Services
 from deepface import DeepFace
 import timeit
+from deepface.basemodels import OpenFace
 
 app = Flask(__name__)
 ser = FR_Services()
-global model
+my_model = OpenFace.loadModel()
+graph = tf.get_default_graph()
 # app.config['Upload_Images'] = Upload
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    global graph
     if request.method == 'POST':
         if request.files:
             if (request.files['unknown_image'] and not request.files['new_image']):
@@ -17,7 +21,8 @@ def home():
                 print(unknown_image.filename)
                 unknown_image.save(os.path.join('upload_Images', unknown_image.filename))
                 st_time = timeit.default_timer()
-                label, dist = ser.face_recognize(unknown_image.filename, model)
+                with graph.as_default():
+                    label, dist = ser.face_recognize(unknown_image.filename, model)
                 end_time = timeit.default_timer()
                 print('Elapsed Time: ', end_time - st_time)
                 return render_template('index.html', label=label, dist=dist)
@@ -35,5 +40,4 @@ def home():
 
 
 if __name__ == '__main__':
-    model = DeepFace.build_model('OpenFace')
     app.run(debug=True)
